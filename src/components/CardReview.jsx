41811bd8bf13b34dc3befa0202a4d57d7f533b01
Caio Padilha <caiopadilha@id.uff.br>
2025-03-12 17:17:17 -0300

    Estrelas nos comentários feitos e data format

--- src/components/CardReview.jsx
+++ src/components/CardReview.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import "./CardReview.css"
+import starFilled from '../assets/green-star.svg'
+import starEmpty from '../assets/gray-star.svg'
 export default function CardReview () {
 
     const [data, setData] = useState([]);
@@ -11,17 +13,31 @@ export default function CardReview () {
     }, [])
     return (
         <ul className="comentarios">
-        {data.map(review => (
+        {data.length === 0 ? (
+          <p>Sem avaliações até o momento.</p>
+        ) : data.map(review => (
             <>
               <li key={review.id}>
               <p className="nome">{review.name}</p>
-              <p className="data-postagem">{review.created_at}</p>
-              <p className="nota">Nota: {review.stars}</p>
+              <p className="data-postagem">
+                {
+                  new Date(review.created_at).toLocaleString(
+                    'pt-BR',
+                    { day: 'numeric', month: 'long', year: 'numeric' }
+                  )
+                }
+              </p>
+              <div>
+                {Array.from({ length: review.stars}).map((_, index) =>  (
+                  <img src={starFilled} alt="Green star" key={index} />
+                ))}
+                {Array.from({ length: 5 - review.stars}).map((_, index) =>  (
+                  <img src={starEmpty} alt="Gray star" key={index}/> 
+                ))}
+              </div>
               <p className="descricao">{review.description}</p>
               </li>
-              <hr />
             </>
-
         ))}
       </ul>       
     )
--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -3,21 +3,15 @@ import "./ReviewForm.css"
 export default function ReviewForm() {
     return(
         <div className="container-review">
-            <h1>Deixe seu review</h1>
+            <h3>Deixe seu review</h3>
 
             <form className="review-form">
                 <label htmlFor="name">Nome</label>
                 <input type="text" id="name" name="name" required />
                 <label htmlFor="review">Review</label>
-                <textarea id="review" name="review" required rows={6}></textarea>
-                <div>
-                    <p>Estrelas</p>
-                    {[1, 2, 3, 4, 5].map((star) => (
-                        <>
-                        <input type="radio" id={star} name="rating" value={star} required />
-                        <label htmlFor={star}>{star}</label>   
-                        </>
-                    ))}
+                <textarea id="review" name="review" required rows={6} className="text-area"></textarea>
+                <div className="form-group">
+                    <label htmlFor="stars">Estrelas</label>
                 </div>
                 <button type="submit">Enviar</button>
             </form>
